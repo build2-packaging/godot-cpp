3c8d37b0c93894192055c69a75ecb54dd3166c9b
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-05-25 03:33:06 +0200

    Fixed exported include paths

--- tests/basics/driver.cpp
+++ tests/basics/driver.cpp
@@ -1,38 +1,5 @@
-// #include <sstream>
-// #include <stdexcept>
 
-// #include <godot-cpp/version.hpp>
-// #include <godot-cpp/godot-cpp.hpp>
-
-// #undef NDEBUG
-// #include <cassert>
-
-// int main ()
-// {
-//   using namespace std;
-//   using namespace godot_cpp;
-
-//   // Basics.
-//   //
-//   {
-//     ostringstream o;
-//     say_hello (o, "World");
-//     assert (o.str () == "Hello, World!\n");
-//   }
-
-//   // Empty name.
-//   //
-//   try
-//   {
-//     ostringstream o;
-//     say_hello (o, "");
-//     assert (false);
-//   }
-//   catch (const invalid_argument& e)
-//   {
-//     assert (e.what () == string ("empty name"));
-//   }
-// }
+#include <godot_cpp/godot.hpp>
 
 int main() {}
 
